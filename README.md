c6481bc4b7bc4f293fbfcb282a94811627feff87
xiao-baii <55825595+xiao-baii@users.noreply.github.com>
2021-10-24 18:59:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# bai-index
+
+​	目前开发环境下，备案号无法正常显示在底部。
+
+​	生产环境下可以正常显示
+
+​    有冗余 px
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/introduction/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="introduction">
+    <p>
+      <b>Bai</b>
+    </p>
+    <ul class="intro-list">
+      <li>
+        <a href="https://blog.mua.blue" target="_blank">博客</a>
+      </li>
+      <li>
+        <a href="https://seat.mua.blue" target="_blank">座位预约</a>
+      </li>
+      <li>
+        <a href="https://chaoxing.mua.blue" target="_blank">超星签到</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+.introduction {
+  padding-top: 0px;
+  p {
+    font-size: 15px;
+    line-height: 2em;
+    margin: 0;
+    letter-spacing: 2px;
+    text-decoration: none;
+    transition: color .2s ease;
+    padding-top:12px;
+  }
+  .intro-list{
+    align-self: center;
+    color: rgba(51, 51, 51, .62);
+    display: flex;
+    justify-content: center;
+    padding:0;
+    li{
+      margin: 6px;
+      list-style: none;
+      opacity: 1;
+      transform:translateY(10px);
+      a{
+        font-size: 13px;
+        display: inline-block;
+        transition: all .5s;
+        position: relative;
+        text-decoration: none;
+        color:inherit;
+        &:after{
+          transition: all .5s;
+          content: "";
+          position: absolute;
+          bottom: -1px;
+          left: 0;
+          width: 0;
+          height: 1px;
+          background-color:rgba(51, 51, 51, .54);
+        }
+        &:hover{
+          color:#f33;
+          &:after{
+            width:100%;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
