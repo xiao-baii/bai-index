2fe7559d318721a8bbe7979b9d695ea052fde0ec
xiao-baii <55825595+xiao-baii@users.noreply.github.com>
2021-10-29 20:47:01 +0800

    备案号组件化

---
+++ src/components/beian/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="beian">
+    <a href="http://beian.miit.gov.cn" target="_blank">鄂ICP备2021014854号-1</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="scss">
+.beian{
+  font-size: 13px;
+  position:absolute;
+  text-align:center;
+  bottom:0px;
+  width:80%; /* 正常情况应该是100% */
+  a {
+    font-size: 13px;
+    text-decoration: none;
+    color: rgba(51, 51, 51, .54);
+    &:hover{
+       color:#f33;
+      &:after{
+         width:100%;
+       }
+    }
+  }
+}
+</style>
